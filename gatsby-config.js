e65acb73ce3fcafc086a9a4ac4d1a405e72ee7ca
jonataspinto <jonatas.pferreira@b2wdigital.com>
2020-08-06 19:50:53 -0300

    chora: atualiza configuracoes

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,10 +30,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/sobre-mim/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#26647A`,
+        theme_color: `#26647A`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/profile-peq.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-styled-components`,
