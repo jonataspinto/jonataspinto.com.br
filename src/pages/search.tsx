a79d9b2ce3d361f2d1e810d418adde029a9666ec
Jonatas Pinto (Afya) <jonatas.ferreira@afya.com.br>
2025-11-14 17:42:10 -0300

    feat: add SearchPage component with layout and SEO

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import { Layout, Search } from '../components/modules';
 import { SEO } from '../components/features';
 
-const SearchPage = () => (
-  <Layout>
-    <Search />
-  </Layout>
-);
-
-export default SearchPage;
+export default function SearchPage() {
+  return (
+    <Layout>
+      <Search />
+    </Layout>
+  );
+}
 
 export const Head = () => <SEO title="Search" />;
