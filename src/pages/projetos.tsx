48639ca680a13f522509c17a59fea0761fc3eaae
Jonatas Pinto (Afya) <jonatas.ferreira@afya.com.br>
2025-11-14 17:40:10 -0300

    feat: create ProjectsPage component with layout and SEO

--- src/pages/projetos.tsx
+++ src/pages/projetos.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import { Layout, ProjectsBox } from '../components/modules';
 import { SEO } from '../components/features';
 
-const ProjectsPage = () => (
-  <Layout>
-    <ProjectsBox />
-  </Layout>
-);
-
-export default ProjectsPage;
+export default function ProjectsPage() {
+  return (
+    <Layout>
+      <ProjectsBox />
+    </Layout>
+  );
+}
 
 export const Head = () => <SEO title="Projetos" />;
